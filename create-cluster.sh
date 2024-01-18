26cf95d3b198c866609326ba0af8c5190deae348
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-19 07:32:30 +0900

    2024.01

---
+++ create-cluster.sh
@@ -0,0 +1,19 @@
+echo ""
+echo "=== create Cluster & Ingress-Nginx (Ingress Controller) ==="
+echo "[create] cluster creating..."
+kind create cluster --name kustomize-sample-cluster --config=kustomize-sample-cluster.yml
+sleep 30
+
+echo ""
+echo "[create] create ingress-nginx"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+
+echo ""
+echo "[wait] wait ingress-nginx standby"
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+sleep 20
